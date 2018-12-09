e54ddc1230fcc256000e9d7fdf87314c894b0e5e
cham-s <cc.attou@gmail.com>
2018-12-09 22:16:59 +0100

    displaying images on device

--- Challenge3/ViewController.swift
+++ Challenge3/ViewController.swift
@@ -21,8 +21,23 @@ class ViewController: UICollectionViewController {
         
         let items = try! fm.contentsOfDirectory(atPath: path)
         images = items.filter { $0.hasSuffix("jpg") }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Image", for: indexPath) as! ImageCell
+        cell.imageView.image = UIImage(named: images[indexPath.row])
+        cell.name.text = images[indexPath.row]
+        cell.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
-        print(images)
+        return cell
+        
     }
     
 }
---
+++ ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  Challenge3
+//
+//  Created by chams on 09/12/2018.
+//  Copyright © 2018 Chams. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
